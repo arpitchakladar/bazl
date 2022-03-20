4c7d64e40fdc41eeafad2752e0b8ed232c152b53
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2022-03-20 22:03:17 +0530

    Added script to convert a file to its bazl file system counterpart.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,3 @@
-#!/bin/sh
 PARAMS="-B build"
 LINKER="$(whereis -b i386-elf-ld | awk 'NF>1{print $NF}')"
 COMPILER="$(whereis -b i386-elf-gcc | awk 'NF>1{print $NF}')"
---
+++ scripts/convert-bazl-file.sh
@@ -0,0 +1,28 @@
+file_name=$(mktemp /tmp/bazl-file-name.XXX)
+file_desc=$(mktemp /tmp/bazl-file.XXX)
+file_content=$(mktemp /tmp/bazl-file-content.XXX)
+
+cat $1 > $file_content
+
+file_size=$(stat -c%s "$1")
+sec_count=$((file_size / 512))
+
+if [ $file_size -gt $((sec_count * 512)) ]
+then
+	sec_count=$((sec_count + 1))
+fi
+
+echo $2
+
+echo $2 > $file_name
+truncate -s 128 $file_name
+cat $file_name > $file_desc
+echo -en $(printf '\\x%x' $sec_count) >> $file_desc
+echo -en $(printf '\\x%x' 1) >> $file_desc
+truncate -s 512 $file_desc
+cat $file_desc > $1
+cat $file_content >> $1
+truncate -s $(((sec_count + 1) * 512)) $1
+
+rm $file_name
+rm $file_desc
