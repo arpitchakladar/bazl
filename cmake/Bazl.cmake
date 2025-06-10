926fad99a37275e142aa0befd4cbcf47e8cef552
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2025-06-11 03:23:03 +0530

    Using kebab case instead of snake case for file names.

--- cmake/Bazl.cmake
+++ cmake/Bazl.cmake
@@ -2,6 +2,6 @@ set(bazl_TARGET_FILE "${CMAKE_CURRENT_BINARY_DIR}/bazl")
 
 add_custom_target(bazl
 	ALL
-	DEPENDS kernel boot_loader
-	COMMAND cat $<TARGET_FILE:boot_loader> $<TARGET_FILE:kernel> > ${bazl_TARGET_FILE} && truncate -s ${KERNEL_SIZE_KB}k ${bazl_TARGET_FILE}
+	DEPENDS kernel boot-loader
+	COMMAND cat $<TARGET_FILE:boot-loader> $<TARGET_FILE:kernel> > ${bazl_TARGET_FILE} && truncate -s ${KERNEL_SIZE_KB}k ${bazl_TARGET_FILE}
 	VERBATIM)
