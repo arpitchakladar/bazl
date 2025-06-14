12b3eeb6cff3f710ff2000fe20b4475242702ac8
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2025-06-14 13:57:08 +0530

    Cleaned up CMake configuration.

--- cmake/Bazl.cmake
+++
@@ -1,7 +0,0 @@
-set(bazl_TARGET_FILE "${CMAKE_CURRENT_BINARY_DIR}/bazl")
-
-add_custom_target(bazl
-	ALL
-	DEPENDS kernel boot-loader
-	COMMAND cat $<TARGET_FILE:boot-loader> $<TARGET_FILE:kernel> > ${bazl_TARGET_FILE} && truncate -s ${KERNEL_SIZE_KB}k ${bazl_TARGET_FILE}
-	VERBATIM)
--- kernel/cmake/C.cmake
+++
@@ -1,9 +0,0 @@
-set(CMAKE_C_COMPILER i686-elf-gcc)
-set(CMAKE_C_FLAGS "-ffreestanding -m32 -g")
-if ("${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
-	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O0")
-elseif ("${CMAKE_BUILD_TYPE}" STREQUAL "Release")
-	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -O3")
-endif ()
-
-set(CMAKE_C_LINK_EXECUTABLE ${CMAKE_LINK_EXECUTABLE})
